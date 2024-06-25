d3ea2713f389a0ff3ba3b426ff9d5342d28b85c7
SAADAOUI ABDESSALEM <107399601+abdessalems@users.noreply.github.com>
2024-06-25 22:18:59 +0200

    Update validator.sh with new comments

--- validator.sh
+++ validator.sh
@@ -1,56 +1,54 @@
 #!/bin/bash
-# This is a bash script for validating Docker images and containers.
 # Ce fichier vérifie si le conteneur est bien en lecture seule et que l'utilisateur n'est pas root 
 
 set -euo pipefail
-# The 'set' command is used to change the values of shell options and set the positional parameters.
-# '-e' option will cause the shell to exit if any invoked command fails.
-# '-u' option treats unset variables and parameters as an error.
-# '-o pipefail' option sets the exit code of a pipeline to that of the rightmost command to exit with a non-zero status.
+# Le 'set' commande configure les options du shell.
+# '-e' fait que le shell se termine si une commande échoue.
+# '-u' traite les variables non définies comme des erreurs.
+# '-o pipefail' fait que le code de sortie d'un pipeline est celui de la dernière commande échouée.
 
-IMG=$(echo img$$) #crée une image unique à partir de l'identifiant du processus du shell 
-# Creating a unique image name using the process ID of the current shell.
+IMG=$(echo img$$) # Crée une image unique à partir de l'identifiant du processus du shell 
 
-docker image build --tag $IMG ./src # --load  #creation et identification d'une image docker 
-# Building a Docker image from the Dockerfile located in the ./src directory.
-# The built image is tagged with the unique name generated above.
-# The '--load' option ensures the built image is loaded into the Docker daemon only if you use docker buildx.
+docker image build --tag $IMG ./src # --load  # Crée et identifie une image Docker 
+# Construit une image Docker à partir du Dockerfile situé dans le répertoire ./src.
+# L'image construite est taguée avec le nom unique généré ci-dessus.
+# L'option '--load' charge l'image construite dans le démon Docker uniquement si vous utilisez docker buildx.
 
-USR=$(docker container run --rm --entrypoint=whoami $IMG ) #on insére dans la variable USR la valeur du user contenu dans l'image
-# Running a Docker container from the image built above.
-# The container is removed after its execution (--rm option).
-# The entrypoint of the container is overridden to execute the 'whoami' command.
-# The output of 'whoami' (which is the username) is stored in the USR variable.
+USR=$(docker container run --rm --entrypoint=whoami $IMG) # Insère dans la variable USR la valeur de l'utilisateur contenu dans l'image
+# Exécute un conteneur Docker à partir de l'image construite ci-dessus.
+# Le conteneur est supprimé après son exécution (option --rm).
+# Le point d'entrée du conteneur est remplacé par la commande 'whoami'.
+# La sortie de 'whoami' (qui est le nom d'utilisateur) est stockée dans la variable USR.
 
-if [[ $USR == "root" ]]; then  #si le user contenu dans l'image s'avére être root alors on interompt le processus et on affiche un message d'erreur
-echo "User cannot be root!"
+if [[ $USR == "root" ]]; then  # Si l'utilisateur contenu dans l'image est root, affiche un message d'erreur
+    echo "User cannot be root!"
 fi
-# Checking if the user inside the container is root.
-# If it is, an error message is printed.
+# Vérifie si l'utilisateur à l'intérieur du conteneur est root.
+# Si c'est le cas, un message d'erreur est affiché.
 
 docker container run --rm --detach --tmpfs /tmp --read-only $IMG > /dev/null
-# Running a Docker container in detached mode from the image built above.
-# The container is removed after its execution (--rm option).
-# A temporary filesystem is mounted at /tmp inside the container (--tmpfs option).
-# The filesystem of the container is mounted as read-only (--read-only option).
-# The output of this command is redirected to /dev/null to suppress it.
+# Exécute un conteneur Docker en mode détaché à partir de l'image construite ci-dessus.
+# Le conteneur est supprimé après son exécution (option --rm).
+# Un système de fichiers temporaire est monté sur /tmp à l'intérieur du conteneur (option --tmpfs).
+# Le système de fichiers du conteneur est monté en lecture seule (option --read-only).
+# La sortie de cette commande est redirigée vers /dev/null pour la supprimer.
 
 ID=$(docker container ls -laq)
-# Getting the ID of the last created container.
+# Obtient l'ID du dernier conteneur créé.
 
 RUNNING=$(docker container inspect -f '{{.State.Status}}' $ID)
-# Checking the status of the container with the ID obtained above.
-# The status is extracted from the output of 'docker container inspect' command using a format string.
+# Vérifie l'état du conteneur avec l'ID obtenu ci-dessus.
+# L'état est extrait de la sortie de la commande 'docker container inspect' en utilisant une chaîne de formatage.
 
 if [[ $RUNNING == "running" ]]; then
     docker kill $ID > /dev/null
 else
-echo "Container cannot run in read-only mode!"
+    echo "Container cannot run in read-only mode!"
 fi
-# Checking if the container is running.
-# If it is, the container is killed.
-# If it's not, an error message is printed.
+# Vérifie si le conteneur est en cours d'exécution.
+# Si c'est le cas, le conteneur est tué.
+# Sinon, un message d'erreur est affiché.
 
 docker rmi $IMG > /dev/null
-# Removing the Docker image built above.
-# The output of this command is redirected to /dev/null to suppress it.
+# Supprime l'image Docker construite ci-dessus.
+# La sortie de cette commande est redirigée vers /dev/null pour la supprimer.
