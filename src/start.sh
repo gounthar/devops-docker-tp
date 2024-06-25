8c2ba711d963815064f7abf3e426176eff583503
antoinecaby caby <acaby@yipikay.fr>
2024-06-25 16:45:23 +0000

    Modification des fichiers Dockerfile et start.sh pour qu'ils soient compatibles avec validator.sh

--- src/start.sh
+++ src/start.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-# This script is used to create a timestamp file and write the current date and time into it.
+# Ce script est utilisé pour créer un fichier de timestamp dans un répertoire temporaire et y écrire la date et l'heure actuelles.
 
-set -eux
+set -eu
 # Setting shell options for better error handling and debugging.
 # 'e' option will cause the shell to exit if any invoked command fails.
 # 'u' option treats unset variables and parameters as an error.
-# 'x' option prints each command that gets executed to the terminal, useful for debugging.
+# Ici on retire l'option 'x' comme demandé dans le raedme
 
-touch /home/www/started.time
-# This command creates a file named 'started.time' in the '/home/www' directory.
-# If the file already exists, the command does not change the file but updates its access and modification timestamps.
+# On crée un répertoire temporaire en écriture seule pour éviter les problèmes avec le mode lecture seule
+mkdir -p /tmp/www
+
+# On crée le fichier de timestamp dans le répertoire temporaire que l'on vient de créer juste au dessus
+touch /tmp/www/started.time
 
 if [ $? -ne 0 ]; then
     exit
@@ -17,9 +19,10 @@ fi
 # This conditional statement checks the exit status of the last command (touch command in this case).
 # If the exit status is not zero, which indicates an error, the script will exit immediately.
 
-date > /home/www/started.time
+date > /tmp/www/started.time
 # This command writes the current date and time to the 'started.time' file.
 # The '>' operator is used to redirect the output of the 'date' command to the file.
+# Ici, on change le répertoire de "home/www/" à "tmp/www/" car c'est là que ce trouve le fichier que l'on vient de créer
 
 exec "$@"
 # This command replaces the current shell process with the command given as argument.
