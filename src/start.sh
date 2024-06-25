0c4a77d79409377dcbc96e6f2c86013235ced81d
Asmae-Mabrouk <145761334+Asmae-Mabrouk@users.noreply.github.com>
2024-06-25 21:35:22 +0200

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,27 +1,20 @@
 #!/bin/sh
-# This script is used to create a timestamp file and write the current date and time into it.
+# Ce script est utilisé pour créer un fichier de timestamp et y écrire la date et l'heure actuelles.
 
-set -eux
-# Setting shell options for better error handling and debugging.
-# 'e' option will cause the shell to exit if any invoked command fails.
-# 'u' option treats unset variables and parameters as an error.
-# 'x' option prints each command that gets executed to the terminal, useful for debugging.
+set -eu
+# 'e' fait en sorte que le shell quitte en cas d'erreur.
+# 'u' traite les variables non définies comme des erreurs.
 
-touch /home/www/started.time
-# This command creates a file named 'started.time' in the '/home/www' directory.
-# If the file already exists, the command does not change the file but updates its access and modification timestamps.
+# Crée un fichier nommé 'started.time' dans le répertoire temporaire.
+touch /tmp/started.time
 
+# Si la commande touch échoue, le script s'arrête.
 if [ $? -ne 0 ]; then
     exit
 fi
-# This conditional statement checks the exit status of the last command (touch command in this case).
-# If the exit status is not zero, which indicates an error, the script will exit immediately.
 
-date > /home/www/started.time
-# This command writes the current date and time to the 'started.time' file.
-# The '>' operator is used to redirect the output of the 'date' command to the file.
+# Écrit la date et l'heure actuelles dans le fichier 'started.time'.
+date > /tmp/started.time
 
+# Remplace le processus shell actuel par la commande fournie en argument.
 exec "$@"
-# This command replaces the current shell process with the command given as argument.
-# It's used to pass control from the script to another program or command.
-# The "$@" is a special variable that holds all the arguments passed to the script.
